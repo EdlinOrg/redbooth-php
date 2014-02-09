845b1db56599a5471fcc14e2bae43045897c77f1
Carl Asman <github@edlin.org>
2014-02-09 19:31:02 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
+Shameless self-plugging
+-----------------------
+I am a freelancing software developer (M.Sc.) that has been working fulltime for over 15 years now in various fields
+
+See
+
+http://www.edlin.org/
+
+for info on my background. Please feel free to contact me if you are looking for developers.
+I prefer to work remotely on projects.
+
+That said, let's carry on with some notes about this code:
+
 redbooth-php
 ============
 
-php code to call the Redbooth (Teambox) api
+Basic php code to call the Redbooth (Teambox) api.
+
+I needed to create tasks in Redbooth from Php, 
+I don't have time/interest to flesh it out at the moment hence the code is very basic.
+
+In my [teambox-bugtracker-bridge][teambox-bugtracker-bridge] code, in Python, you will see loads of other calls I make to the api, which you easily can adapt to be in php
+
+
+[teambox-bugtracker-bridge]:https://github.com/EdlinOrg/teambox-bugtracker-bridge
---
+++ src/RedBooth.php
@@ -0,0 +1,95 @@
+<?php 
+
+class Redbooth {
+
+    private $redbooth_username = 'TODO';
+    private $redbooth_password = 'TODO';
+
+    private $redbooth_projectid = 'TODO';
+    private $redbooth_tasklistid = 'TODO';
+
+    private $baseurl = 'https://redbooth.com/api/2/';
+
+    public function createTask($title, $description)
+    {
+        $comment= array(
+                        'body' => $description
+        );
+    
+        $args = array(
+                        'name' => $title,
+                        'project_id' => $this->redbooth_projectid,
+                        'task_list_id' => $this->redbooth_tasklistid,
+                        'comments_attributes' => array($comment)
+        );
+    
+        $projectid=$this->redbooth_projectid;
+        $tasklistid=$this->redbooth_tasklistid;
+    
+        $data = $this->connectionHelper('tasks', $args, 'POST');
+    
+        return $data;
+    }
+    
+    public function createComment($taskid, $comment)
+    {
+        $args = array(
+                        'comments_attributes' =>
+                        array(
+                                        array(
+                                                        'body' => $comment,
+                                        )
+                        )
+        );
+    
+        $data = $this->connectionHelper('projects/' . $this->redbooth_projectid . '/tasks/' . $taskid , $args, 'PUT');
+    
+        return $data;
+    }
+    
+    private function connectionHelper($urlpart, $args, $requestType)
+    {
+        $args = json_encode($args);
+        $url = $this->baseurl . $urlpart;
+        $ch = curl_init();
+
+        switch($requestType)
+        {
+            case 'POST':
+                break;
+            case 'PUT':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+                break;
+            default:
+                echo "not supported";
+        }
+
+        $username= $this->redbooth_username;
+        $password= $this->redbooth_password;
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $args );
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 45);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $mydata = curl_exec ($ch);
+
+        if (curl_errno($ch))
+        {
+            $err_str = 'Failed to retrieve url [' . curl_error($ch) . ']' . "\n";
+
+            echo $err_str;
+
+            return false;
+        }
+        return $mydata;
+    }
+
+}
+
+?>
